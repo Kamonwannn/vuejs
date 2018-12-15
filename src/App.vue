9ca42335b69b87aee9abea646c16ea1fdf45c485
kamonwan <kamonwan.5034@gmail.com>
2018-12-15 17:18:02 +0700

    supply

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,9 @@
     
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/Suppliers">Product</router-link> |
-      <router-link to="/user">User</router-link>
+    
+      <router-link to="/Suppliers">Suppliers</router-link> |
+      
     </div>
     <router-view/>
   </div>
---
+++ src/views/Suppliers.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <h1>Order</h1>
+ <b-table striped hover :items="orders" :fields="fields" :per-page="pagesize" :current-page="pageindex"></b-table>
+  <b-pagination size="lg" :total-rows="orders.length" v-model="pageindex" :per-page="pagesize">  </b-pagination>
+    </div>
+</template>
+<script>
+import axios from "axios";
+export default {
+  name: "order",
+  components: {},
+  data() {
+    return {
+      message: " Final Project ",
+      orders: [],
+      pagesize:10,
+      pageindex:1,
+      fields: [
+        { key: "supplier_id", sortable: true },
+        { key: "company_name", sortable: true },
+        { key: "contact_name", sortable: true },
+        { key: "contact_title", sortable: true },
+        { key: "address", sortable: true },
+        { key: "city", sortable: true },
+        { key: "region", sortable: true },
+        { key: "postal_code", sortable: true },
+        { key: "country", sortable: true },
+        { key: "phone", sortable: true },
+        { key: "fax", sortable: true }
+        
+      ]
+    };
+  },
+  mounted() {
+    var instace = this;
+    axios
+      .get("https://frozen-river-13804.herokuapp.com/api/Suppliers/")
+      .then(function(response) {
+        console.log(response.data);
+        instace.orders = response.data.data;
+      });
+  }
+};
+</script>
